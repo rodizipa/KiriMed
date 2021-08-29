be1bc911ad79345b6c267cb3dda921988acf4398
rodrigopadilha <rodrigo.padilha@viasoft.com.br>
2021-08-29 14:31:24 -0300

    Start commit

---
+++ src/main/java/br/com/kirinus/kirimed/KiriMedApplication.java
@@ -0,0 +1,13 @@
+package br.com.kirinus.kirimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiriMedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiriMedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/kirinus/kirimed/KiriMedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kirinus.kirimed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiriMedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
