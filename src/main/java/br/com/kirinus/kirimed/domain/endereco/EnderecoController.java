6a442edf94eadebeb0d716ab4832c02ffd2de281
rodrigopadilha <rodrigo.padilha@viasoft.com.br>
2021-08-30 00:27:32 -0300

    #7 Ajustes de Porta de Servidor, path de contexto e Path de controllers

--- src/main/java/br/com/kirinus/kirimed/domain/endereco/EnderecoController.java
+++ src/main/java/br/com/kirinus/kirimed/domain/endereco/EnderecoController.java
@@ -3,9 +3,11 @@ package br.com.kirinus.kirimed.domain.endereco;
 import br.com.kirinus.kirimed.framework.CrudRestController;
 import br.com.kirinus.kirimed.framework.JpaCrudService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("enderecos")
+@RestController
+@RequestMapping("enderecos")
 @RequiredArgsConstructor
 public class EnderecoController extends CrudRestController<Endereco, Long> {
 
